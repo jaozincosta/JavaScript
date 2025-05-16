5b6d6baa5c4ca9357806dd5767372b6ab853996d
João Costa <138248786+jaozincosta@users.noreply.github.com>
2025-05-15 23:51:46 -0300

    Aprendizado de variaveis e erros

---
+++ escopo.js
@@ -0,0 +1,8 @@
+let estudante = 'Amanda';
+
+if (1 > 0) {
+    estudante = 'João';
+    console.log(estudante);
+}
+
+console.log(estudante);
---
+++ variaveis.js
@@ -0,0 +1,14 @@
+let numero = 10;
+let nome = "Joao";
+
+if (numero === 10) {
+  console.log("O número é 10.");
+} else {
+  console.log("O número não é 10.");
+}
+
+if (nome === "Alice") {
+  console.log("Boas vindas, Joao!");
+} else {
+  console.log("Você não é a Joao.");
+}
